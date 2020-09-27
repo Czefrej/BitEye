6334771d1a1b9215cb4033de17b03f4fa8aa6dd8
Czefrej <jeffery@outerbest.pl>
2020-09-27 06:24:38 +0200

    Initial commit

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Offer extends Model
 {
     use HasFactory;
+    protected $table = "offers";
+    protected $primaryKey = "id";
+    protected $keyType = "bigint";
+    public $incrementing = false;
+    public $timestamps = false;
 }
