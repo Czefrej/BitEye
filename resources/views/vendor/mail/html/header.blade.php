f1ba78a9c927b26922ea72af37194083683dc49a
Wiktor Jeffery <tavrosteam@gmail.com>
2020-12-08 14:57:26 +0100

    Modified Email Templates

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,8 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="{{asset("assets/biteye.svg")}}" height="100px" class="logo" alt="Laravel Logo">
+@if (trim($slot) === 'BitEye')
+<img src="http://biteye.pl/assets/biteye.png" height="100px" class="logo" alt="BitEye Logo">
 @else
 {{ $slot }}
 @endif
